517ce42bca63622f68ffe87c7a3700729cff0a3a
nftwisdom <108710448+nftwisdom@users.noreply.github.com>
2022-08-24 10:25:16 +0700

    Update dataActions.js

--- src/redux/data/dataActions.js
+++ src/redux/data/dataActions.js
@@ -25,9 +25,9 @@ export const fetchData = () => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let mintedSupply = await store
+      let totalSupply = await store
         .getState()
-        .blockchain.smartContract.methods.mintedSupply()
+        .blockchain.smartContract.methods.totalSupply()
         .call();
       // let cost = await store
       //   .getState()
@@ -36,7 +36,7 @@ export const fetchData = () => {
 
       dispatch(
         fetchDataSuccess({
-          mintedSupply,
+          totalSupply,
           // cost,
         })
       );
