62f83bceb7c519d0e6b6088ee234faec29158935
nftwisdom <108710448+nftwisdom@users.noreply.github.com>
2022-08-27 10:00:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import org.json.*;
+
 import React, { useEffect, useState, useRef } from "react";
+import org.json.JSONObject;
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
