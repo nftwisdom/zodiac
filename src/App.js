d250a8a52ab99b15b034a951d5246883c0fd5b34
nftwisdom <108710448+nftwisdom@users.noreply.github.com>
2022-08-27 13:48:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ function App() {
     let PLcost = CONFIG.PUBLIC_COST;
     let WLcost = CONFIG.WL_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
-    let account = String(blockchain.account);
+    let account = blockchain.account;
     console.log(account);
     let obj = JSON.parse(JSON.stringify(SIGNATURE));
     console.log("Signature:",typeof obj, obj);
