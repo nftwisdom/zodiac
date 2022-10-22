e9012cbcbef62e751a5b0f694e6a38395510497b
nftwisdom <108710448+nftwisdom@users.noreply.github.com>
2022-10-22 12:46:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .PublicClaim(mintID)
+      .PublicMint(mintID)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
